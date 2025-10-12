ff56b82719429db0432e354d250649db1fc3d972
lic-connect <sunilvasarkar@outlook.com>
2025-10-12 21:49:51 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'lic-connect-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.html',
+  '/signup.html',
+  '/licnew.jpg',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+  // Add any other important files here!
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
